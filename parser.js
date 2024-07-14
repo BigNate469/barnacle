c6642d3c631aa4a6d72bb5721a700f02f299fea7
BigNate469 <owen.morgan09@icloud.com>
2024-07-14 16:21:44 -0400

    Update parser.js

--- parser.js
+++ parser.js
@@ -5,19 +5,15 @@ let barnacle = {
     let currentKeyName = "";
     let currentValue = "";
     let currentArray = [];
-    let currentObject = {};
     for (let i = 0; i < text.length; i++) {
       if (text.at(i) === ";") {
         if (current === "arrayValue") {
           currentValue = currentArray;
-        } else if (current === "objectValue") {
-          currentValue = currentObject;
         };
         current = "key";
         Object.defineProperty(output, currentKeyName, {value: currentValue});
         currentValue = "";
         currentArray = [];
-        currentObject = {};
       if (current === "key") {
         if (text.at(i) === " ") {
           current = "find";
@@ -30,9 +26,6 @@ let barnacle = {
           currentValue = text.at(i);
         } else if (text.at(i + 1) === "a") {
           current = "arrayValue";
-          i++;
-        } else if (text.at(i + 1) === "o") {
-          current = "objectValue";
           i++;
         };
       } else if (current === "value") {
