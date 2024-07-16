985464c4a08edb0d3075eca579c59269137f4984
BigNate469 <owen.morgan09@icloud.com>
2024-07-16 07:51:15 -0400

    Update newparser.js

--- newparser.js
+++ newparser.js
@@ -4,16 +4,16 @@ let parseBarnacle = function(text) {
   let currentValue = "";
   let currentMode = "key";
   for (let i = 0; i < text.length; i++) {
-    if (text.at(i) === ";" && !(text.at(i - 1) === "|")) {
+    if (text.at(i) === ";" && !(text.at(i - 1) === "\\")) {
       output[currentKey] = currentValue;
       currentMode = "key";
       currentValue = "";
       currentKey = "";
-    } else if (text.at(i) === " " && !(text.at(i - 1) === "|")) {
+    } else if (text.at(i) === " " && !(text.at(i - 1) === "\\")) {
       currentMode = "value";
-    } else if (currentMode === "key" && !(text.at(i - 1) === "|")) {
+    } else if (currentMode === "key" && !(text.at(i - 1) === "\\")) {
       currentKey = currentKey + text.at(i);
-    } else if (currentMode === "value" && !(text.at(i - 1) === "|")) {
+    } else if (currentMode === "value" && !(text.at(i - 1) === "\\")) {
       currentValue = currentValue + text.at(i);
     };
   };
