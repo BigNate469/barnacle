233d08be753f32c157c872a15570a063e5ff9813
BigNate469 <owen.morgan09@icloud.com>
2024-07-16 17:26:17 -0400

    Update newparser.js

--- newparser.js
+++ newparser.js
@@ -1,12 +1,15 @@
 let parseBarnacle = function(text) {
   let output = {};
+  let tempKeys = [];
+  let tempValues = [];
   let currentKey = "";
   let currentValue = "";
   let currentMode = "key";
   for (let i = 0; i < text.length; i++) {
     let i2 = i - 1;
     if (text.at(i) === ";" && !(text.at(i2) === "|")) {
-      output[currentKey] = currentValue;
+      tempKeys.push(currentKey);
+      tempValues.push(currentValue);
       currentMode = "key";
       currentValue = "";
       currentKey = "";
@@ -16,6 +19,14 @@ let parseBarnacle = function(text) {
       currentKey = currentKey + text.at(i);
     } else if (currentMode === "value" && !(text.at(i) === "|")) {
       currentValue = currentValue + text.at(i);
+    };
+  };
+  for (let i = 0; i < tempKeys.length; i++) {
+    let temp = tempValues[i];
+    if(temp.includes("|a")) {
+      for (let i2 = 3; i2 < temp.length; i2++) { // Starts at the fourth character because this is where the array should acually start
+        //TODO: parse arrays.
+      };
     };
   };
   return output;
