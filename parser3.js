5f3f766f841137e4d9d97f3065a187302cae1bdb
BigNate469 <owen.morgan09@icloud.com>
2024-08-16 13:29:07 -0400

    Update parser3.js

--- parser3.js
+++ parser3.js
@@ -1,1 +1,7 @@
-
+const parseBarnacle = function(text) {
+  const lines = text.split(";");
+  for (let i = 0; i < lines.length; i++) {
+    let value = lines[i].split(/(?<!\\) /);
+    return value;
+  }
+}
